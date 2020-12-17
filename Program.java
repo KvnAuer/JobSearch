dfc0e020f6ec91f6644fb6a72ed27135bb50597c
KvnAuer <kza0067@auburn.edu>
2020-12-17 13:41:31 -0600

    I finished setting up API connection and was successful on test

--- Program.java
+++ Program.java
@@ -1,4 +1,6 @@
+import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Program {
@@ -9,8 +11,9 @@ public class Program {
     {
 
 
-
-        String myUrl = "https://api.indeed.com/ads/apisearch?"
+        try {
+            
+            String myUrl = "https://api.indeed.com/ads/apisearch?"
         + "publisher=123412341234123&"
         + "q=java+developer&l=austin%2C+tx&sort=&"
         + "radius=&st=&jt=&start=&limit=&"
@@ -18,6 +21,24 @@ public class Program {
         + "userip=1.2.3.4&"
         + "useragent=Mozilla/%2F4.0%28Firefox%29&v=2";
         URL url = new URL(myUrl);
+        connection = (HttpURLConnection) url.openConnection();
+
+        //Request setup
+        connection.setRequestMethod("GET");
+        connection.setConnectTimeout(5000);
+        connection.setReadTimeout(5000);
+
+
+        int status = connection.getResponseCode();
+        System.out.println(status);
+
+        } catch (MalformedURLException e) {
+            //TODO: handle exception;
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
         System.out.println("Hello World");
     }
 }
